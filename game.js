dee1f9e5091d4399b0c0aaad22823c9439539dce
Sergiy Kuzmich <nika246458246459@gmail.com>
2020-03-13 05:06:53 +0200

    add gravitation and earth

--- game.js
+++ game.js
@@ -1,3 +1,128 @@
+
+class Bird {
+    constructor(params) {
+        this.ctx = params.ctx;
+        this.div = params.div;
+        this.sx = params.sx;
+        this.sy = params.sy;
+        this.sw = params.sw;
+        this.sh = params.sh;
+        this.x = params.x;
+        this.y = params.y;
+        this.width = params.width;
+        this.height = params.height;
+
+        this.speed_gravitaion = 1.22;
+        this.valueToUp = 85;
+
+        this.frame = 0;
+        this.frameCount = 0;
+
+        this.started = false;
+        this.lockTimer = false;
+
+        this.bird_img = new Image();
+        this.bird_img.src = '/images/sprite-bird.png';
+
+        this.load(this);
+        this.animation(this);
+
+
+    }
+
+    load(params) {
+        params.bird_img.onload = () => {
+            params.ctx.drawImage(params.bird_img, params.sx, params.sy, params.sw, params.sh, params.x, params.y, params.width, params.height);
+        };
+
+    }
+
+    animation(params) {
+
+        window.requestAnimationFrame(function anim() {
+            //FPS
+            // params.frameCount++;
+            // if (params.frameCount < 10) {
+            //     window.requestAnimationFrame(anim);
+            //     return;
+            // }
+            // params.frameCount = 0;
+
+            params.frame++;
+            params.frame = params.frame >= 4 ? 0 : params.frame;
+            params.sx = 34 * params.frame;
+            // if (!params.started) {
+            //     params.y += 5;
+            //     if (params.y > 330) params.y -= 10;
+            // }
+            params.ctx.clearRect(params.x, params.y - 50, params.width, 115);
+            params.ctx.drawImage(
+                params.bird_img,
+                params.sx, params.sy,
+                params.sw, params.sh,
+                params.x, params.y,
+                params.width, params.height);
+            window.requestAnimationFrame(anim);
+        });
+        if (!params.started) {
+            document.addEventListener('keydown', (e) => {
+                if (e.key == ' ') {
+                    if (!this.started) {
+                        this.turn_on_gravitation();
+                    }
+                    this.started = true;
+                }
+            });
+        }
+
+    }
+
+
+    turn_on_gravitation() {
+
+        let id = setInterval(() => {
+            this.y += this.speed_gravitaion;
+            if (this.y > this.div.clientHeight - 115) {
+                clearInterval(id);
+            }
+        }, 1);
+
+    }
+    move_up_bird() {
+        let new_y = 0;
+        new_y = this.y - this.valueToUp;
+        if (new_y > -150) {
+            let id = setInterval(() => {
+                this.y -= this.speed_gravitaion * 2;
+                if (this.y < new_y) {
+                    clearInterval(id);
+                }
+            }, 1);
+        }
+    }
+}
+class Earth {
+    constructor(params) {
+        this.ctx = params.ctx;
+        this.earth_img = new Image();
+        this.earth_img.src = '/images/spr_earth.png';
+        this.x = params.x;
+        this.y = params.y;
+        this.loadEarth(this);
+    }
+    loadEarth(params) {
+        params.earth_img.onload = () => {
+            params.ctx.drawImage(params.earth_img, params.x, params.y);
+
+            for(let i=0; i<23; i++){
+                params.x+=20;
+                params.ctx.drawImage(params.earth_img, params.x, params.y);
+            }
+        };
+    }
+}
+
+
 class Game {
     constructor() {
 
@@ -7,89 +132,25 @@ class Game {
         canv.height = div.clientHeight;
         canv.width = div.clientWidth;
 
+        let bird = new Bird({ div: div, ctx: ctx, sx: 0, sy: 0, sw: 34, sh: 40, x: 190, y: 325, width: 50, height: 60 });
+        document.addEventListener('keydown', (e) => {
+            if (e.key == ' ') {
+                bird.move_up_bird();
+            }
+        });
 
-        this.loadBird(ctx);
-
+        let earth = new Earth({ ctx: ctx, x: 0, y: 530 });
     }
 
-      loadBird(ctx) {
-        const bird_img = new Image();
-        let frame = 0;
 
-        let sx = 0, sy = 0, sw = 34, sh = 40, dx = 190, dy = 325, dw = 50, dh = 60;
-        bird_img.src = '/images/sprite-bird.png';
-        bird_img.onload = () => {
-
-            setInterval(function(){
-                ++frame;
-                sx = frame * 34;
-                if(frame==4){
-                    frame=0;
-                }
-              
-
-                ctx.drawImage(bird_img, sx, sy, sw, sh, dx, dy, dw, dh);
-               
-        },130);
-        };
-        
-        // requestAnimationFrame(function birdAnimation(time) {
-        //     requestAnimationFrame(birdAnimation);
-        //     ctx.drawImage(bird_img, sx, sy, sw, sh, dx, dy, dw, dh);
-            
-        //     frame++;
-        //     sx = 34 * frame;
-            
-        //     if (frame > 4) {
-        //         sx = 0;
-        //         frame = 0;
-        //     }
-        // });
-        
-            
-        
-
-            
-           
-        
-    }
 }
 
 
 
 let game = new Game();
-// //images
-//  const bird_img = new Image();
-//  bird_img.src = '/images/sprite-bird.png';
-// // bird_img.onload = () => {
-// //     ctx.drawImage(bird_img,0,0,33,40,120,325,60,60);
-// //   };
 
-// class Sprite{
-//     constructor(options){
-//         this.ctx = options.ctx;
-//         this.image = options.image;
-//         this.width = options.width;
-//         this.height = options.height;
-//         this.numberOfFrames = options.numberOfFrames;
-//         this.ticksPerFrame = options.ticksPerFrame;
 
-//         this.start();
-//     }
 
-//     start(){
-//         alert(this.height);
-//     }
-// }
-
-// let sprite = new Sprite({
-//     ctx: ctx,
-//     image: bird_img,
-//     width:60,
-//     height:60,
-//     numberOfFrames: 4,
-//     ticksPerFrame: 4
-// });
 
 
 
